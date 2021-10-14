f976c31faafda774c436363da17190c4b5e77e68
jibundeyare <contact@jibundeyare.com>
2021-10-14 17:33:38 +0200

    Improve page title
    The home page did not display any title.
    Now the home page display the name of the website.

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <title><?php wp_title(); ?></title>
+        <title><?php if (is_front_page() || is_home()): echo get_bloginfo('name'); else: echo wp_title(''); endif ?></title>
         <link rel="shortcut icon" type="image/png" href="<?= get_stylesheet_directory_uri(); ?>/img/icons8-bulle.svg" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <?php wp_head(); ?>
